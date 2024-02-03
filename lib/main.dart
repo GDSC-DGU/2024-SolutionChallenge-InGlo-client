c9a24c22f179ce3d8bb8a056dfb33bc6794935b2
eunji <bian87@dgu.ac.kr>
2024-02-03 19:30:27 +0900

    REFACTOR: appbar -> navigation bar sample refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       //  debugShowCheckedModeBanner: false, //오른쪽 상단에 배너 띠를 없애준다.
-      home: Scaffold(
-          appBar: PreferredSize(
-            // preferredSize를 지정해주지 않으면 에러가 발생한다.
-            preferredSize: const Size.fromHeight(50),
-            child: NavigationAppBar(),
-          ),
-          body: Center(
-            child: LoginPage(),
-          )),
+      home: MainScreen(), // 화면의 표시될 스크린
     );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  // screen 이동 상태 관리
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0; // 현재 선택된 탭의 인덱스
+
+
+  final List<Widget> _pages = [
+    // 각 탭에 해당하는 페이지 위젯 추가
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      // item을 클릭하면 해당 인덱스로 selectedIndex를 변경한다.
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: NavigationAppBar(
+        currentIndex: _selectedIndex, // index 전송
+        onItemTapped: _onItemTapped, // 함수 전송
+      ),
+    );;
   }
 }
--- lib/models/appbar/appbar.dart
+++ lib/models/appbar/appbar.dart
@@ -1,45 +1,33 @@
+// custom_navigation_bar.dart
 import 'package:flutter/material.dart';
 
 class NavigationAppBar extends StatelessWidget {
-  const NavigationAppBar({super.key});
+  final int currentIndex;
+  final Function(int) onItemTapped;
+
+  NavigationAppBar({required this.currentIndex, required this.onItemTapped});
 
   @override
   Widget build(BuildContext context) {
-// 화면 시작
-    return AppBar(
-// 앱 바 메인 텍스트
-        title: const Text('sample'),
-        leading: IconButton(
-          icon: const Icon(Icons.home),
-          tooltip: 'home',
-          onPressed: () {
-            // 홈으로 이동
-          },
+    return BottomNavigationBar(
+      // bottom navigation bar의 items로 index를 관리한다.
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
         ),
-// 첫 번째 아이콘 버튼 배치되어 있음.
-        actions: <Widget>[
-          IconButton(
-              icon: const Icon(Icons.notifications),
-              tooltip: 'alert', // icon 설명 tooltip
-              onPressed: () {
-// 클릭 이벤트 추가 필요
-              }),
-// 프로필
-          IconButton(
-            icon: const Icon(Icons.account_circle),
-            tooltip: 'profile',
-            onPressed: () {
-// nativator 사용, return Scaffold가 반환하는 값을 user 페이지로 한다. (import로 조정하면 될듯)
-              Navigator.push(context, MaterialPageRoute<void>(
-                builder: (BuildContext context) {
-                  return Scaffold(
-                      // 요기 profile 페이지를 추가한다.
-                      );
-                },
-              ));
-            },
-          ),
-        ],
-      );
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Search',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          label: 'Profile',
+        ),
+      ],
+      currentIndex: currentIndex, // 선택된 index
+      selectedItemColor: Colors.blue, // 선택 item color
+      onTap: onItemTapped, // 콜백 함수
+    );
   }
 }
