d48ef0b6f140a7e96bd88a006460b67832058b7b
eunji <bian87@dgu.ac.kr>
2024-02-05 02:39:32 +0900

    FEAT: google map 연동 성공

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import GoogleMaps  // Add this import
+import flutter_config // add env
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +10,11 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // TODO: Add your Google Maps API key
+    // env 파일 사용
+        GMSServices.provideAPIKey(FlutterConfigPlugin.env(for: "MAP_KEY"))
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
