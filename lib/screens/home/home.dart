e2456babb50da260ba82622d0a3662b9087e5329
eunji <bian87@dgu.ac.kr>
2024-02-05 00:59:19 +0900

    FEAT: homepage 파일 생성

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+
+// homepage 생성
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
