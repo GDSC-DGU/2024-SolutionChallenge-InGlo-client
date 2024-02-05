96ff6a29903339786590dd65481220a18fa895ad
nakyong82 <nakyong7770@gmail.com>
2024-02-05 17:23:37 +0900

    Feat: 이슈 리스트 레이아웃 구현

--- lib/screens/issuelist/issulist.dart
+++ lib/screens/issuelist/issulist.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:inglo/screens/issuelist/widgets/issue_item.dart';
 import 'package:inglo/screens/issuelist/widgets/issue_search.dart';
 import 'package:inglo/screens/issuelist/widgets/issue_slider.dart';
 
 class IssueListPage extends StatefulWidget {
   // 나중에 폰트 스타일 다시 재정의!
   static const titleStyle =
-      TextStyle(fontSize: 30, color: Colors.black, fontWeight: FontWeight.w700);
+      TextStyle(fontSize: 25, color: Colors.black, fontWeight: FontWeight.w700);
 
   @override
   State<IssueListPage> createState() => _IssueListPageState();
@@ -15,26 +16,30 @@ class _IssueListPageState extends State<IssueListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              IssueSearch(),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: const Text("Recommand", style: IssueListPage.titleStyle),
-              ),
-              IssueSlider(),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: const Text("Today's Issue",
-                    style: IssueListPage.titleStyle),
-              ),
-            ],
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: SafeArea(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IssueSearch(),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: const Text("Recommand", style: IssueListPage.titleStyle),
+                ),
+                IssueSlider(),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: const Text("Today's Issue",
+                      style: IssueListPage.titleStyle),
+                ),
+                IssueItem(),
+              ],
+            ),
           ),
         ),
       ),
