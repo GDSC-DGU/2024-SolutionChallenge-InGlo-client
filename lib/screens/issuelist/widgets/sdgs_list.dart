303ef2889a78b5395b5f400ac21c1cbeaa4b3db9
nakyong82 <nakyong7770@gmail.com>
2024-02-06 17:43:47 +0900

    FIX: SDGS 선택 페이지 구현

--- lib/screens/issuelist/widgets/sdgs_list.dart
+++ lib/screens/issuelist/widgets/sdgs_list.dart
@@ -2,90 +2,43 @@ import 'package:flutter/material.dart';
 
 // 더미데이터
 final List<Map<String, String>> itemData = [
-  {
-    "title": "1. No Poverty",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
-  {
-    "title": "2. Zero Hunger",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
-  {
-    "title": "3. Good Health And Well-Being",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
-  {
-    "title": "4. Quality Education",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
-  {
-    "title": "5. Gender Equality",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
-  {
-    "title": "6. Clean Water And Sanitation",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
+  {"title": "1. No Poverty", "image": "assets/image/sdgs1.png"},
+  {"title": "2. Zero Hunger", "image": "assets/image/sdgs2.png"},
+  {"title": "3. Good Health And Well-Being", "image": "assets/image/sdgs2.png"},
+  {"title": "4. Quality Education", "image": "assets/image/sdgs2.png"},
+  {"title": "5. Gender Equality", "image": "assets/image/sdgs2.png"},
+  {"title": "6. Clean Water And Sanitation", "image": "assets/image/sdgs2.png"},
   {
     "title": "7. Affordable And Clean Energy",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
+    "image": "assets/image/sdgs2.png"
   },
   {
     "title": "8. Decent Work And Economic Growth",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
+    "image": "assets/image/sdgs2.png"
   },
   {
     "title": "9. Industry, Innovation And Infrastructure",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
+    "image": "assets/image/sdgs2.png"
   },
-  {
-    "title": "10. Reduced Inequalities",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
+  {"title": "10. Reduced Inequalities", "image": "assets/image/sdgs2.png"},
   {
     "title": "11. Sustainable Cities And Communities",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
+    "image": "assets/image/sdgs2.png"
   },
   {
     "title": "12. Responsible Consumption And Production",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
+    "image": "assets/image/sdgs2.png"
   },
-  {
-    "title": "13. Clime Action",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
-  {
-    "title": "14. Life Below Water",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
-  {
-    "title": "15. Life On Land",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
-  },
+  {"title": "13. Clime Action", "image": "assets/image/sdgs2.png"},
+  {"title": "14. Life Below Water", "image": "assets/image/sdgs2.png"},
+  {"title": "15. Life On Land", "image": "assets/image/sdgs2.png"},
   {
     "title": "16. Peace, Justice And Strong Institutions",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
+    "image": "assets/image/sdgs2.png"
   },
   {
     "title": "17. Partnerships For The Goals",
-    "image":
-    "https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80"
+    "image": "assets/image/sdgs2.png"
   },
 ];
 
@@ -94,47 +47,27 @@ class SDGSList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-      child: Column(
-        children: itemData.map((item) => Container(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(
-              Radius.circular(10.0),
-            ),
-            border: Border.all(
-              width: 1,
-              color: Colors.black,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Color(0xFFFFD691),
-                blurRadius: 0.0,
-                spreadRadius: 0.0,
-                offset: Offset(4, 4),
-              ),
-            ],
-          ),
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Text(item["title"]!),
-                ),
-                ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-                  child: Image.network(item["image"]!,
-                    fit: BoxFit.cover, width: 50.0, height: 50.0,),
-                )
-              ],
-            ),
-          ),
-        ),).toList(),
+    return GridView.builder(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      itemCount: itemData.length,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
       ),
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(itemData[index]["image"]!),
+            ),
+          ),
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+          ),
+        );
+      },
     );
   }
 }
