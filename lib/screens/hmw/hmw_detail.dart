17eac3951aa475b87df86314ac80f275cd14448d
nakyong82 <nakyong7770@gmail.com>
2024-02-13 14:55:56 +0900

    FEAT: problem definition 선택 페이지 레이아웃 개발

---
+++ lib/screens/hmw/hmw_detail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HMWDetailPage extends StatelessWidget {
+  const HMWDetailPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
