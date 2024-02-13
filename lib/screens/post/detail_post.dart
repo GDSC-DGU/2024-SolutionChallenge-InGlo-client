d7a8da00a0a99bdc7e6a52ce5e38ad884e5065d8
eunji <bian87@dgu.ac.kr>
2024-02-14 01:44:51 +0900

    FEAT: 댓글 모달 레이아웃 구현(기존)

--- lib/screens/post/detail_post.dart
+++ lib/screens/post/detail_post.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:inglo/screens/post/widgets/image_slider.dart';
 import 'package:inglo/screens/post/widgets/post_user.dart';
-import 'package:inglo/util/dummy/users_sketch.dart';
-import 'package:inglo/widgets/dropdown/writingdropdown.dart';
+import 'package:inglo/widgets/modal/barmodal.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class DetailPost extends StatelessWidget {
   const DetailPost({super.key});
@@ -10,79 +10,100 @@ class DetailPost extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, // This ensures alignment starts from the left
+      body: Container(
+        height:  MediaQuery.of(context).size.height,
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    onPressed: () {
-                      Navigator.pop(context); // 이전 페이지로 이동
-                    },
-                  ),
-                  Spacer(),
-                  IconButton(
-                    icon: Icon(Icons.bookmark), // 여기서 원하는 아이콘을 선택합니다.
-                    color: Color(0xFFFF6280), // 아이콘 색상
-                    onPressed: () {
-                      // 버튼이 눌렸을 때 수행할 작업
-                    },
-                    iconSize: 30.0,
-                  ),
-                ],
-              ),
-            ),
-            Divider(
-              thickness: 1,
-            ),
-            Padding( // Using Padding for consistency and to apply padding
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            SingleChildScrollView(
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start, // Adjusts alignment to the left
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // This ensures alignment starts from the left
                 children: [
-                  SizedBox(
-                    height: 200,
-                    child: ImageSlider(),
-                  ),
-                  SizedBox(height: 20),
-                  Text('Title',
-                    style: TextStyle(
-                      fontSize: 26, // 폰트 크기 설정
-                      fontWeight: FontWeight.bold, // 폰트 굵기 설정
-                      color: Colors.black, // 텍스트 색상 설정
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.arrow_back_ios),
+                          onPressed: () {
+                            Navigator.pop(context); // 이전 페이지로 이동
+                          },
+                        ),
+                        Spacer(),
+                        IconButton(
+                          icon: Icon(Icons.bookmark), // 여기서 원하는 아이콘을 선택합니다.
+                          color: Color(0xFFFF6280), // 아이콘 색상
+                          onPressed: () {
+                            // 버튼이 눌렸을 때 수행할 작업
+                          },
+                          iconSize: 30.0,
+                        ),
+                      ],
                     ),
                   ),
-                  Text('description',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black, // 텍스트 색상 설정
-                    ),
-                  ),
-                  SizedBox(height: 10,),
                   Divider(
                     thickness: 1,
                   ),
-                  SizedBox(
-                    height: 30,
-                    child: PostUser(),
-                  ),
-                  Text('content',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.black, // 텍스트 색상 설정
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          height: 200,
+                          child: ImageSlider(),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Title',
+                          style: TextStyle(
+                            fontSize: 26, // 폰트 크기 설정
+                            fontWeight: FontWeight.bold, // 폰트 굵기 설정
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
+                        ),
+                        Text(
+                          'description',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Divider(
+                          thickness: 1,
+                        ),
+                        SizedBox(
+                          height: 30,
+                          child: PostUser(),
+                        ),
+                        Text(
+                          'content',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
+            ),
+            Positioned(
+              bottom: -20,
+              left: 0,
+              right: 0,
+              child: SizedBox(
+                height: 100,
+                child: BarModal(),
+              ),// 여기서 BarModalWidget은 바텀 모달의 컨텐츠를 표시하는 위젯입니다.
             ),
           ],
         ),
       ),
     );
   }
-
 }
