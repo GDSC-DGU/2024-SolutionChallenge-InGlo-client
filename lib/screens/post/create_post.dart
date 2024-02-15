cae400a1e02bff8acbeaa4d1af6a5d5efa0c47cd
eunji <bian87@dgu.ac.kr>
2024-02-16 04:20:15 +0900

    FEAT: create post html editor 추가

--- lib/screens/post/create_post.dart
+++ lib/screens/post/create_post.dart
@@ -1,90 +1,138 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:inglo/util/dummy/users_sketch.dart';
-import 'package:inglo/widgets/dropdown/writingdropdown.dart';
+import 'package:html_editor_enhanced/html_editor.dart';
+import 'package:file_picker/file_picker.dart';
 
-class CreatePost extends StatelessWidget {
+class CreatePost extends StatefulWidget {
   const CreatePost({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  _CreatePostState createState() => _CreatePostState();
+}
 
+class _CreatePostState extends State<CreatePost> {
+  String result = '';
+  final HtmlEditorController controller = HtmlEditorController();
+  TextEditingController textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: Icon(Icons.arrow_back_ios),
-                    onPressed: () {
-                      Navigator.pop(context); // 이전 페이지로 이동
-                    },
-                  ),
-                  Text('Create Post',
-                    style: TextStyle(
-                      fontSize: 16, // 폰트 크기 설정
-                      fontWeight: FontWeight.bold, // 폰트 굵기 설정
-                      color: Colors.black, // 텍스트 색상 설정
-                    ),),
-                  Spacer(),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFFFD691)),
-                    onPressed: () {},
-                    child: Text('Post',
-                      style: TextStyle(
-                        fontSize: 16, // 폰트 크기 설정
-                        fontWeight: FontWeight.bold, // 폰트 굵기 설정
-                        color: Colors.white, // 텍스트 색상 설정
-                      ),),
-                  )
-                ],
-              ),
-            ),
-            Divider(
-              thickness: 1,
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: Form(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 45.0,
-                      child: WritingCustomDropdown(options: usersSketchData),),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      maxLines: null,
-                      maxLength: 50, // 사용자가 입력할 수 있는 최대 문자 수
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Add Title...",
-                        contentPadding: EdgeInsets.all(0),
-                        hintStyle: TextStyle(fontSize: 20),
-                        isDense: true,
-                      ),
-                    ),
-                    const SizedBox(height: 0), // 두 입력 필드 사이의 공간 추가
-                    TextFormField(
-                      maxLines: null, // 무제한 줄 입력 허용
-                      keyboardType: TextInputType.multiline, // 멀티라인 입력 활성화
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Add Content...",
-                        contentPadding: EdgeInsets.all(0),
-                        hintStyle: TextStyle(fontSize: 14),
-                        isDense: true,
-                      ),
-                    ),
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context); // 이전 페이지로 이동
+          },
+        ),
+        actions: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFFFD691)),
+            onPressed: () {},
+            child: Text('Post',
+              style: TextStyle(
+                fontSize: 16, // 폰트 크기 설정
+                fontWeight: FontWeight.bold, // 폰트 굵기 설정
+                color: Colors.white, // 텍스트 색상 설정
+              ),),
+          )
+        ],
+      ),
+      body: GestureDetector(
+        onTap: () {
+          if (!kIsWeb) {
+            controller.clearFocus();
+          }
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              HtmlEditor(
+                controller: controller,
+                htmlEditorOptions: HtmlEditorOptions(
+                  hint: 'Add content',
+                  shouldEnsureVisible: true,
+                  //initialText: "<p>text content initial, if any</p>",
+                ),
+                htmlToolbarOptions: HtmlToolbarOptions(
+                  defaultToolbarButtons: [
+                    FontButtons(clearAll: false, ), // font style
+                    StyleButtons(), // h1, h2...
+                    InsertButtons(audio: false, video: false, otherFile: false, table: true, hr: true),
+                  ],
+                  customToolbarButtons: [
+                    // 커스텀 툴바
                   ],
                 ),
+                // 이외 사용자 옵션들
+                otherOptions: OtherOptions(height: MediaQuery.of(context).size.height),
+                callbacks: Callbacks(
+                  onBeforeCommand: (String? currentHtml) {
+                    print('html before change is $currentHtml');
+                  },
+                  onChangeContent: (String? changed) {
+                    print('content changed to $changed');
+                  },
+                  onChangeCodeview: (String? changed) {
+                    print('code changed to $changed');
+                  },
+                  onChangeSelection: (EditorSettings settings) {
+                    print('parent element is ${settings.parentElement}');
+                    print('font name is ${settings.fontName}');
+                  },
+                  onEnter: () {
+                    print('enter/return pressed');
+                  },
+                  onFocus: () {
+                    print('editor focused');
+                  },
+                  onBlur: () {
+                    print('editor unfocused');
+                  },
+                  onBlurCodeview: () {
+                    print('codeview either focused or unfocused');
+                  },
+                  onInit: () {
+                    print('init');
+                  },
+                  onImageUploadError: (FileUpload? file, String? base64Str, UploadError error) {
+                    print(describeEnum(error));
+                    print(base64Str ?? '');
+                    if (file != null) {
+                      print(file.name);
+                      print(file.size);
+                      print(file.type);
+                    }
+                  },
+                  onKeyDown: (int? keyCode) {
+                    print('$keyCode key downed');
+                    print('current character count: ${controller.characterCount}');
+                  },
+                  onKeyUp: (int? keyCode) {
+                    print('$keyCode key released');
+                  },
+                  onMouseDown: () {
+                    print('mouse downed');
+                  },
+                  onMouseUp: () {
+                    print('mouse released');
+                  },
+                  onNavigationRequestMobile: (String url) {
+                    print(url);
+                    return NavigationActionPolicy.ALLOW;
+                  },
+                  onPaste: () {
+                    print('pasted into editor');
+                  },
+                  onScroll: () {
+                    print('editor scrolled');
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
