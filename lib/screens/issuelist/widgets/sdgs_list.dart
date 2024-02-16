c24dce79c2a52a3e08a39259209d3cc7f234dbcc
nakyong82 <nakyong7770@gmail.com>
2024-02-16 21:42:34 +0900

    FIX: SDGS 이미지 모두 추가

--- lib/screens/issuelist/widgets/sdgs_list.dart
+++ lib/screens/issuelist/widgets/sdgs_list.dart
@@ -3,43 +3,43 @@ import 'package:inglo/screens/problem_definition/problem_definition.dart';
 
 // 더미데이터
 final List<Map<String, String>> itemData = [
-  {"title": "1. No Poverty", "image": "assets/image/sdgs1.png"},
-  {"title": "2. Zero Hunger", "image": "assets/image/sdgs2.png"},
-  {"title": "3. Good Health And Well-Being", "image": "assets/image/sdgs2.png"},
-  {"title": "4. Quality Education", "image": "assets/image/sdgs2.png"},
-  {"title": "5. Gender Equality", "image": "assets/image/sdgs2.png"},
-  {"title": "6. Clean Water And Sanitation", "image": "assets/image/sdgs2.png"},
+  {"title": "1. No Poverty", "image": "assets/image/sdgs/sdgs1.png"},
+  {"title": "2. Zero Hunger", "image": "assets/image/sdgs/sdgs2.png"},
+  {"title": "3. Good Health And Well-Being", "image": "assets/image/sdgs/sdgs3.png"},
+  {"title": "4. Quality Education", "image": "assets/image/sdgs/sdgs4.png"},
+  {"title": "5. Gender Equality", "image": "assets/image/sdgs/sdgs5.png"},
+  {"title": "6. Clean Water And Sanitation", "image": "assets/image/sdgs/sdgs6.png"},
   {
     "title": "7. Affordable And Clean Energy",
-    "image": "assets/image/sdgs2.png"
+    "image": "assets/image/sdgs/sdgs7.png"
   },
   {
     "title": "8. Decent Work And Economic Growth",
-    "image": "assets/image/sdgs2.png"
+    "image": "assets/image/sdgs/sdgs8.png"
   },
   {
     "title": "9. Industry, Innovation And Infrastructure",
-    "image": "assets/image/sdgs2.png"
+    "image": "assets/image/sdgs/sdgs9.png"
   },
-  {"title": "10. Reduced Inequalities", "image": "assets/image/sdgs2.png"},
+  {"title": "10. Reduced Inequalities", "image": "assets/image/sdgs/sdgs10.png"},
   {
     "title": "11. Sustainable Cities And Communities",
-    "image": "assets/image/sdgs2.png"
+    "image": "assets/image/sdgs/sdgs11.png"
   },
   {
     "title": "12. Responsible Consumption And Production",
-    "image": "assets/image/sdgs2.png"
+    "image": "assets/image/sdgs/sdgs12.png"
   },
-  {"title": "13. Clime Action", "image": "assets/image/sdgs2.png"},
-  {"title": "14. Life Below Water", "image": "assets/image/sdgs2.png"},
-  {"title": "15. Life On Land", "image": "assets/image/sdgs2.png"},
+  {"title": "13. Clime Action", "image": "assets/image/sdgs/sdgs13.png"},
+  {"title": "14. Life Below Water", "image": "assets/image/sdgs/sdgs14.png"},
+  {"title": "15. Life On Land", "image": "assets/image/sdgs/sdgs15.png"},
   {
     "title": "16. Peace, Justice And Strong Institutions",
-    "image": "assets/image/sdgs2.png"
+    "image": "assets/image/sdgs/sdgs16.png"
   },
   {
     "title": "17. Partnerships For The Goals",
-    "image": "assets/image/sdgs2.png"
+    "image": "assets/image/sdgs/sdgs17.png"
   },
 ];
 
@@ -49,7 +49,7 @@ class SDGSList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
-      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
       itemCount: itemData.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 3,
