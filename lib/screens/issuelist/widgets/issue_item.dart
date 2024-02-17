d6b31106b2cb68152021513deed7f86c85550ae6
nakyong82 <nakyong7770@gmail.com>
2024-02-17 15:17:50 +0900

    FIX: 이슈 페이지 디자인 수정

--- lib/screens/issuelist/widgets/issue_item.dart
+++ lib/screens/issuelist/widgets/issue_item.dart
@@ -56,18 +56,22 @@ class IssueListData extends StatelessWidget {
         child: Container(
           margin: const EdgeInsets.symmetric(vertical: 10),
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-          decoration: const BoxDecoration(
+          decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.all(
               Radius.circular(10.0),
             ),
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
             boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10.0,
-                spreadRadius: 0.0,
-                offset: Offset(5, 5),
-              ),
+              // BoxShadow(
+              //   color: Colors.grey,
+              //   blurRadius: 10.0,
+              //   spreadRadius: 0.0,
+              //   offset: Offset(5, 5),
+              // ),
               BoxShadow(
                 color: Color(0xFFFFD691),
                 blurRadius: 0.0,
@@ -92,7 +96,7 @@ class IssueListData extends StatelessWidget {
                         children: [
                           Icon(
                             Icons.favorite,
-                            color: Color(0xFFFF6E80),
+                            color: Color(0xFFD7A859),
                             size: 20.0,
                           ),
                           Text("12"),
