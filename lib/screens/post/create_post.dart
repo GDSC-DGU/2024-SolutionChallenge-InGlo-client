620af3429fbb40ccd2f0211af4f47c94f41728a9
eunji <bian87@dgu.ac.kr>
2024-02-22 21:25:18 +0900

    REFACTOR: profile api 완료 후 navigate 추가

--- lib/screens/post/create_post.dart
+++ lib/screens/post/create_post.dart
@@ -49,7 +49,7 @@ class _CreatePostState extends State<CreatePost> {
 
   // post 생성 api
   Future<void> _handlePost() async {
-    print('선택 sdgs : $sdgs');
+    print('선택 sdgs : $sdgs 선택 sketch : $sketch_id');
     Provider.of<PostSDGS>(context, listen: false).setSDGS(int.parse('$sdgs')); // sdgs 저장
     final url = "https://dongkyeom.com/api/v1/posts/";
 
